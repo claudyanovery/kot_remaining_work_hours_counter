45d02930e25d1df127e8e3169dbab57532ed114b
Claudya Novery <claudyanovery@Claudyas-MacBook-Pro.local>
2021-01-09 11:31:24 +0900

    Bug fix

--- background.js
+++ background.js
@@ -1,42 +1,4 @@
-
-let jpDay;
-const getJapaneseDay = (date) => {
-    switch(date) {
-        case 1:
-            jpDay = "（月）";
-            break;
-        case 2:
-            jpDay = "（火）";
-            break;
-        case 3:
-            jpDay = "（水）";
-            break;
-        case 4:
-            jpDay = "（木）";
-            break;
-        case 5:
-            jpDay = "（金）";
-            break;
-        case 6:
-            jpDay = "（土）";
-            break;
-        case 7:
-            jpDay = "（日）";
-            break;
-    }
-};
-
-let d = new Date();
-let day = getJapaneseDay(d.getDay());
-let month_now = (d.getMonth() + 1) < 10 ? `0${d.getMonth() + 1}` : `${d.getMonth() + 1}`;
-let date_now = d.getDate() < 10 ? `0${d.getDate()}` : `${d.getDate()}`;
-let today = month_now + "/" + date_now + jpDay;
-
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({date_today: today.toString()}, function() {
-        console.log("Today is " + today.toString());
-    });
-
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
--- content.js
+++ content.js
@@ -1,19 +1,52 @@
 var rawRecords = {};
+let jpDay;
+
+const getJapaneseDay = (date) => {
+    switch(date) {
+        case 1:
+            jpDay = "（月）";
+            break;
+        case 2:
+            jpDay = "（火）";
+            break;
+        case 3:
+            jpDay = "（水）";
+            break;
+        case 4:
+            jpDay = "（木）";
+            break;
+        case 5:
+            jpDay = "（金）";
+            break;
+        case 6:
+            jpDay = "（土）";
+            break;
+        case 7:
+            jpDay = "（日）";
+            break;
+    }
+};
+
+let d = new Date();
+let day = getJapaneseDay(d.getDay());
+let month_now = (d.getMonth() + 1) < 10 ? `0${d.getMonth() + 1}` : `${d.getMonth() + 1}`;
+let date_now = d.getDate() < 10 ? `0${d.getDate()}` : `${d.getDate()}`;
+let today = month_now + "/" + date_now + jpDay;
 
 function removeChars (text) {
     var tmp = text.replace(/[C]/g, "");
     return tmp.split(/\s+/).filter((el) => { return el; })
 }
-chrome.storage.sync.get('date_today', (data) => {
-    document.querySelectorAll("p").forEach((p) => {
-        if (removeChars(p.innerText)[0] == data.date_today) {            
-            p.parentNode.parentNode.querySelectorAll(".start_end_timerecord, .rest_timerecord").forEach((record) => {
-                rawRecords[record.dataset.htSortIndex] = record.innerText;
-            })
-            chrome.runtime.sendMessage({isLoaded: true, rawRecords: rawRecords}, (response) => {
-                console.log(response);
-            })
-        }
-    })
+
+document.querySelectorAll("p").forEach((p) => {           
+    if (removeChars(p.innerText)[0] == today.toString()) {            
+        p.parentNode.parentNode.querySelectorAll(".start_end_timerecord, .rest_timerecord").forEach((record) => {
+            rawRecords[record.dataset.htSortIndex] = record.innerText;
+        })
+        chrome.runtime.sendMessage({isLoaded: true, rawRecords: rawRecords}, (response) => {
+            console.log(response);
+        })
+    }
 })
+
 
